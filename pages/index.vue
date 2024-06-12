72a81c5f527fb34bf31bacafbd53ca391776d5e0
Shein-Alexander013 <78673939+Shein-Alexander013@users.noreply.github.com>
2024-06-12 22:59:02 +0700

    new site logo, new banners

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@
     </NuxtLink>
     <section class="images">
       <div class="image-container">
-        <NuxtLink to="/catalog/categoryMan" class="dropdown-item"><img src="/assets/images/custom.png" alt="banner-Men"></NuxtLink>
+        <NuxtLink to="/catalog/categoryMan" class="dropdown-item"><img src="/assets/images/for_man.png" alt="banner-Men"></NuxtLink>
       </div>
       <div class="image-container">
-        <NuxtLink to="/catalog/categoryWomen" class="dropdown-item"><img src="/assets/images/banner-woman.png" alt="banner-Woman"></NuxtLink>
+        <NuxtLink to="/catalog/categoryWomen" class="dropdown-item"><img src="/assets/images/for_woman.png" alt="banner-Woman"></NuxtLink>
       </div>
     </section>
   </main>
