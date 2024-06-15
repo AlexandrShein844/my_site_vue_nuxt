72cdef6fe544229f07103cc2e89df08882ccb3a4
Shein-Alexander013 <78673939+Shein-Alexander013@users.noreply.github.com>
2024-06-16 03:19:58 +0700

    new page contact

---
+++ pages/contact/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <title>Контакты</title>
+    <div class="info">
+      <h1 class="info-text">Вы можете связаться с создателем сайта по почте: Al3xandrShein@yandex.ru</h1>
+    </div>
+  </template>
+  
+  <style scoped>
+  .info {
+    text-align: center;
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f9f9f9;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+  
+  .info-text {
+    font-family: 'Arial', sans-serif;
+    font-size: 1.2rem;
+    line-height: 1.6;
+    color: #333;
+    margin: 0;
+  }
+  </style>
+  
